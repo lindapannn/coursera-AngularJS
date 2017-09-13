af333e25b00128b8b32dc64ac1bfa0b23c6790c3
LINDA PAN <lindapuw@gmail.com>
2017-09-13 16:36:28 -0400

    click show item

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -2,25 +2,49 @@
     'use strict';
   
     var app = angular.module('NarrowItDownApp', [])
+    
+    app.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
   
     app.controller('NarrowItDownController', function ($scope, MenuSearchService ) {
-      var service = MenuSearchService;
-      $scope.output = function() {
-        return service.getMatchedMenuItems();
-      }; 
-      
+        var ctrl = this;
+        var service = MenuSearchService;
+        // console.log(this, $scope);
+        ctrl.getItems = function() {
+            service.getMatchedMenuItems()
+            .then(function(result){
+                ctrl.found = result;
+                console.log("after filter");
+            }); 
+        }; 
     });
 
-    app.service('MenuSearchService', function ($http) {
+    app.service('MenuSearchService', function ($http, ApiBasePath) {
         var service = this;
 
-        service.getMatchedMenuItems = function () {
-            return $http.get('https://davids-restaurant.herokuapp.com/menu_items.json').then(function (result) {
+        service.getMatchedMenuItems = function (searchTerm) {
+            return $http.get(ApiBasePath + '/menu_items.json')
+            .then(function (result) {
                 // process result and only keep items that match
-                var foundItems = result.data;
-            
+                var menuList = result.data.menu_items;
+                var foundItems = [];
+
+                if (!searchTerm) {
+                    console.log("input is empty");
+                //     return foundItems;
+                }
+
+                angular.forEach(menuList, function(value, key) {
+                //     if (value.description.toLowerCase().indexOf(searchTerm) !== -1)
+                //         {
+                            foundItems.push(value);
+                //         }
+                });
+                
                 // return processed items
                 return foundItems;
+            })
+            .catch(function(error){
+                return error.data;
             });
         };
     });
